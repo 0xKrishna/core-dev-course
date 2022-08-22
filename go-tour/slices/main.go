ee92e9a7ba034407f0ad45fbadb1b81d8176dd57
Krishna Upadhyaya <krishnau1604@gmail.com>
2022-08-22 20:41:15 +0530

    maps exercise

--- go-tour/slices/main.go
+++ go-tour/slices/main.go
@@ -4,6 +4,7 @@ import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
 	res := make([][]uint8, dy)
+
 	for y := 0; y < dy; y++ {
 		res[y] = make([]uint8, dx)
 		for x := 0; x < dx; x++ {
